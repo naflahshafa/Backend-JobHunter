12d8e014539d9685f8b9513f44e476715ec7309e
Naflah Shafa <11211066@student.itk.ac.id>
2023-12-06 21:58:22 +0800

    update db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,31 +1,31 @@
-const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
+// const Sequelize = require('sequelize');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-const {
-  DATABASE_URL,
-} = process.env;
+// const {
+//   DATABASE_URL,
+// } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
-  define: {
-    timestamps: false,
-  },
-});
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   define: {
+//     timestamps: false,
+//   },
+// });
 
-const testDBConnection = async() => {
-  try {
-    await sequelize.authenticate();
-    console.log("Koneksi ke database berhasil.");
-  } catch (error) {
-    console.error("Gagal terkoneksi ke database:", error);
-  }
-};
+// const testDBConnection = async() => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Koneksi ke database berhasil.");
+//   } catch (error) {
+//     console.error("Gagal terkoneksi ke database:", error);
+//   }
+// };
 
-module.exports = {
-  testDBConnection,
-  development: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "DB_DIALECT",
-  },
-};
+// module.exports = {
+//   testDBConnection,
+//   development: {
+//     use_env_variable: "DATABASE_URL",
+//     dialect: "DB_DIALECT",
+//   },
+// };
